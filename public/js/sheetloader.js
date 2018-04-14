5c719565dbd505e1a525a95a2906cb86580f50ca
mohsinhaider <mohsinshaider@gmail.com>
2018-04-14 11:07:53 -0700

    Immense work on sentence structure and table clearing

--- public/js/sheetloader.js
+++ public/js/sheetloader.js
@@ -29,7 +29,7 @@ function loadCSV(sheetResponseObject) {
         dataType: "text",
         success: function(data) {
             var rowData = data.split(/\r?\n|\r/);
-            var tableData = '<table class="table table-bordered table-striped table-headline headline">';
+            var tableData = '<table id="sheet-table" class="table table-bordered table-striped table-headline headline">';
             for (var count = 0; count < rowData.length; count++) {
                 var cell_data = rowData[count].split(",");
                 tableData += '<tr>';
@@ -50,8 +50,13 @@ function loadCSV(sheetResponseObject) {
     });
 }
 
+
 let speakButton = document.getElementById('speak-button');
 let keeperPrompt = document.getElementById('keeper-prompt');
+let sheetTable = document.getElementById('sheet_table');
+let sheetTableHeaders = sheetTable.getElementsByTagName("th");
+let sheetTableRows = sheetTable.getElementsByTagName("tr");
+
 speakButton.onclick = function() {
     var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
     recognition.lang = 'en-US';
@@ -60,8 +65,84 @@ speakButton.onclick = function() {
     recognition.start();
 
     recognition.onresult = function(event) {
-        keeperPrompt.innerText = "You said... " + event.results[0][0].transcript;
+        var speechResult = event.results[0][0].transcript;
+        keeperPrompt.innerText = "You said... " + speechResult;
+        var speechResultAdjusted = speechResult.toLowerCase();
+
+        if (speechResultAdjusted.includes("reset")) {
+            location.reload();
+        } else {
+            if (speechResultAdjusted.includes("match")) {
+                let sentenceArray = speechResultAdjusted.split(" ");
+                var afterFilter, afterMatch = false;
+                var columnName;
+                var criteriaString = "";
+
+                for (let i = 0; i < sentenceArray.length; i++) {
+                    if (afterFilter) {
+                        columnName = sentenceArray[i];
+                        afterFilter = false;
+                    }
+                    else if (afterMatch) {
+                        if (sentenceArray[i] === 'by') {
+                            continue;
+                        }
+                        criteriaString += sentenceArray[i];
+                        if (i != sentenceArray.length - 1) {
+                            criteriaString += " ";
+                        }
+                    }
+                    else if (sentenceArray[i] === "keeper") {
+                        // pass
+                    }
+                    else if (sentenceArray[i] === "filter") {
+                        afterFilter = true;
+                        continue;
+                    }
+                    else if (sentenceArray[i] === "match") {
+                        afterMatch = true;
+                        continue;
+                    }
+                }
+
+                console.log('Column: ' + columnName);
+                console.log('Criteria: ' + criteriaString);
+
+                if (columnName.length > 0 && criteriaString.length > 0) {
+                    keeperMatch(columnName, criteriaString);
+                }
+            }
+        }
     };
+}
+
+function keeperMatch(columnName, criteriaString) {
+    let columnMapping = [];
+    let tdIndex;
+    for (let i = 0; i < sheetTableHeaders.length; i++) {
+        let currentColumnName = sheetTableHeaders[i].innerText.toLowerCase();
+        if (currentColumnName === columnName) {
+            tdIndex = i;
+        }
+        columnMapping.push(currentColumnName);
+    }
+
+    console.log(columnMapping);
+    console.log(tdIndex);
+
+    for (var i = 0; i < sheetTableRows.length; i++) {
+        var td = sheetTableRows[i].getElementsByTagName("td")[tdIndex]; // Fix this (iterate by trs)
+        if (td) {
+            if (td.innerText.toLowerCase().trim() === criteriaString) {
+                console.log('made it');
+                sheetTableRows[i].style.display = "";
+            } 
+            else {
+                sheetTableRows[i].innerHTML = "";
+            }
+        }       
+    }
+
 }
 
 loadSheets();
