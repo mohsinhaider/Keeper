64c67457000ef531b37b243787df61ea61f44497
mohsinhaider <mohsinshaider@gmail.com>
2018-04-14 02:35:04 -0700

    Successfully redirected to /sheet with newly assigned ID

--- app/routes/sheet_routes.js
+++ app/routes/sheet_routes.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 let Sheet = mongoose.model('Sheet');
 
 module.exports = function (app) {
+    // Upload a sheet, save it to the database, redirect to its view
     app.post('/sheet/upload', (req, res) => {
         if (req.files) {
             let uploadedSheetFile = req.files.sheetfile;
@@ -25,12 +26,26 @@ module.exports = function (app) {
                 date: today
             });
 
-            newSheet.save((err, sheet) => {
-                if (err) {
-                    res.send(err);
-                }
-            });
+            let sheetBuffer = uploadedSheetFile.data;
 
+            fs.writeFile('public/sheet-files/' + sheetTitle + '.csv', sheetBuffer, (err) => {
+                if (err) { res.send(err); }
+
+                newSheet.save((err, sheet) => {
+                    if (err) {
+                        res.send(err);
+                    }
+
+                    Sheet.findOne({ file_name: sheetTitle }, (err, sheet) => {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            res.redirect('/sheet?sheetID=' + sheet.id);
+                        }
+                    });
+                });
+            });
         }
     });
 }
