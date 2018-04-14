a591abddff15cf8df61fe2cb3ca6e5b25d8b350e
mohsinhaider <mohsinshaider@gmail.com>
2018-04-13 23:14:32 -0700

    Added Schema(s) to server and wrote server start code

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ const upload = require('express-fileupload');
 const app = express();
 const port = 8080;
 
+const mongoose = require('mongoose');
+var Sheet = require('./app/models/sheet');
+var Question = require('./app/models/question');
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 app.use(upload());
 
+mongoose.connect(db.url);
+
+app.listen(port, () => {
+    console.log('Keeper started on ' + port + '!');
+})
